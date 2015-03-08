99538b1645d1761bc8e240d2b1d5a88113801eef
Kyle Baker <finesttype@gmail.com>
2015-03-08 18:25:25 -0500

    work contiunes

--- waitbutwhypage1.js
+++ waitbutwhypage1.js
@@ -1,6 +1,28 @@
 $(document).ready(function(){
 	var ageGuess = prompt("How many years do you think you'll live?")
 	alert(ageGuess)
-	var format = prompt("Show in hours, days, weeks, months, or years?")
-	alert(format)
+	var timeScale = prompt("Show in hours, days, weeks, months, or years?")
+	alert(timeScale)
+
+	function tableMaker(howLong, timeIncrement) {
+		if (timeScale === "years") {
+			var line = 10
+			var row = ageGuess / 10
+		}
+		else if (timeScale === "months") {
+			var line = 12
+			var row = ageGuess / 10 / 12
+		}
+		
+		for (i=0; i<row; i++){
+			document.getElementById("table").innerHTML = line + row;
+		}
+	}
+
+	tableMaker(ageGuess, timeScale);
+
+
+
+
+
 })
