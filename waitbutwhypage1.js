4aabed89ebc1bc1eaa79308f751ed1f4f00dc24f
Kyle Baker <finesttype@gmail.com>
2015-03-08 18:02:23 -0500

    work begins

--- waitbutwhypage1.js
+++ waitbutwhypage1.js
@@ -1,3 +1,6 @@
 $(document).ready(function(){
-	
+	var ageGuess = prompt("How many years do you think you'll live?")
+	alert(ageGuess)
+	var format = prompt("Show in hours, days, weeks, months, or years?")
+	alert(format)
 })
