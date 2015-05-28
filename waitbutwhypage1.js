ea92c31cfc2bd98e97cb33de058610b6745e83df
Kyle Baker <finesttype@gmail.com>
2015-05-28 16:23:41 -0500

    stuff

--- waitbutwhypage1.js
+++ waitbutwhypage1.js
@@ -1,22 +1,4 @@
-$(document).ready(function() {
-	
-	var dir = "E:/userbenchmark_data/dtuff";
-	var fileextension = ".png";
-	$.ajax({
-    //This will retrieve the contents of the folder if the folder is configured as 'browsable'
-    url: dir,
-    success: function (data) {
-        //Lsit all png file names in the page
-        $(data).find("a:contains(" + fileextension + ")").each(function () {
-            var filename = this.href.replace(window.location.host, "").replace("http:///", "");
-            $("body").append($("<img src=" + dir + filename + "></img>"));
-        });
-    }
-});
 
-
-
-	/*
 
 	function newUser() {
 		var ageGuess = prompt("How many years do you think you'll live?"), userName = prompt("Pick a username?"), lifeTable = [];
